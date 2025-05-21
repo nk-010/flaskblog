b15e71e3be59723d63c7336fb1a642be1d5bd71f
nk-010 <nithyakamal76@gmail.com>
2025-05-21 10:53:36 +0100

    Initial commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env file
+load_dotenv()
+
+db= SQLAlchemy()
+bcrypt= Bcrypt()
+login_manager= LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+
+def create_app():
+    app= Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('DATABASE_URI')
+    app.config['SECRET_KEY']= os.getenv('SECRET_KEY')
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app) 
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
