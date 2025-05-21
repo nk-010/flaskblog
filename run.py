453486cf702616eb9f8072e7f719a4a7a387fa7d
nk-010 <nithyakamal76@gmail.com>
2025-05-21 16:07:11 +0100

    Changes for deployment

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from flaskblog import create_app
 
 app= create_app()
 
 if __name__ =='__main__':
-    app.run(host='0.0.0.0', debug= True, use_reloader=False)
+    port= int(os.environ.get("PORT", 4000))
+    app.run(host='0.0.0.0', port= port) 
