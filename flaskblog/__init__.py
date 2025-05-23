7b10a94d116095d86ec8230b0a23fa362ff12008
nk-010 <nithyakamal76@gmail.com>
2025-05-23 10:53:39 +0100

    Fix: changed the loadenv path to render specified path

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
 # Load variables from .env file
-load_dotenv('/etc/secrets/SECRET_KEY.env')
+# load_dotenv('/etc/secrets/SECRET_KEY.env')
 
 db= SQLAlchemy()
 bcrypt= Bcrypt()
@@ -17,8 +17,8 @@ login_manager.login_message_category = 'info'
 
 def create_app():
     app= Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('DATABASE_URI')
-    app.config['SECRET_KEY']= os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URI')
+    app.config['SECRET_KEY']= os.environ.get('SECRET_KEY')
 
     db.init_app(app)
     bcrypt.init_app(app)
